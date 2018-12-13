9d2f86152ecd1856f7adc41bc8c05849547db94b
Diogo M de Oliveira <dmarostega@gmail.com>
2018-12-13 15:26:15 -0200

    Implementação relação com convidados

--- src/main/java/com/eventoapp/controllers/EventoController.java
+++ src/main/java/com/eventoapp/controllers/EventoController.java
@@ -1,14 +1,15 @@
 package com.eventoapp.controllers;
 
-import java.util.ArrayList;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.eventoapp.models.Convidado;
 import com.eventoapp.models.Evento;
+import com.eventoapp.repository.ConvidadoRepository;
 import com.eventoapp.repository.EventoRepository;
 
 @Controller
@@ -16,6 +17,9 @@ public class EventoController {
 	
 	@Autowired 
 	private EventoRepository er;
+	
+	@Autowired
+	private ConvidadoRepository cr;
 	
 	
 	@RequestMapping(value="/cadastrarEvento",method=RequestMethod.GET)
@@ -36,7 +40,29 @@ public class EventoController {
 		Iterable<Evento> eventos = er.findAll();
 		mv.addObject("eventos", eventos);
 				
+		return mv;		
+	}
+	
+//	@RequestMapping("/details")
+	@RequestMapping("/{codigo}")
+	public ModelAndView detailsEvento(@PathVariable("codigo") long codigo) {
+		Evento evento = er.findByCodigo(codigo);
+		ModelAndView mv = new ModelAndView("evento/detailsEvento");
+		mv.addObject("evento", evento);
+		Iterable<Convidado> convidados = cr.findByEvento(evento);
+		mv.addObject("convidados",convidados);
 		return mv;
+	}
+	
+	@RequestMapping(value="/{codigo}",method=RequestMethod.POST)
+	public String detailsEventoPost(@PathVariable("codigo") long codigo, Convidado convidado) {
+		Evento evento = er.findByCodigo(codigo);
+		convidado.setEvento(evento);
+		cr.save(convidado);		
 		
+		//Iterable<Convidado> convidados = cr.findByEvento(evento);
+		//mv.addObject("convidados",convidados);
+		
+		return "redirect:/{codigo}";
 	}
 }
